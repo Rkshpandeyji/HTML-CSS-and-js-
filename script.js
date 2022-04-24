0c2ca69f9e0e63d09a3cc184804ed0872c5402d2
rakesh pandey <rkshpandeyji123@gmail.com>
2022-04-24 16:38:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var q = 4;
+var arr = document.getElementsByClassName("element");
+init();
+
+window.onload =init();
+
+var arr = document.getElementsByClassName("element");
+
+function right() {
+    var can = false;
+    var access = false;
+    var k;
+    var score = document.getElementById("score");
+
+}j
